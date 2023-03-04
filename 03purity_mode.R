1915d77e52bddee8bd8a0523f033c3680a01b301
WangHYLab <42341884+WangHYLab@users.noreply.github.com>
2023-03-04 14:06:21 +0800

    Update 03purity_mode.R

--- 03purity_mode.R
+++ 03purity_mode.R
@@ -2,7 +2,7 @@
 {
 normal_miExpr1 <- read.csv("./testdata/02cancer_healthy_cellline_derived_cpm_exosomes/healthy_cellline_derived_cpm_exosomes.csv")
 normal_cpm_miExpr<-normal_miExpr1
-normal_cpm_miExpr<-normal_cpm_miExpr[,-1]
+normal_cpm_miExpr<-normal_cpm_miExpr[,-1] 
 
 cell_line_cpm_miExpr1 <- read.csv("./testdata/02cancer_healthy_cellline_derived_cpm_exosomes/cancer_cellline_derived_cpm_exosomes1.csv")
 colnames(cell_line_cpm_miExpr1)[1]<- "gene_id"
